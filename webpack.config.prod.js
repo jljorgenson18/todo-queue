45263ad54a1b3b397e93cddf0be9fa1ac8fced3f
jljorgenson18 <jljorgenson18@gmail.com>
2016-11-23 08:38:30 -0600

    Adding webpack stats to production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,37 +1,44 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const StatsPlugin = require('stats-webpack-plugin');
 
 module.exports = {
     cache: false,
     entry: [
-        './src/index',
+        "./src/index",
     ],
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/'
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/static/"
     },
     module: {
         loaders: [{
             test: /\.js$/,
-            loaders: ['babel'],
-            include: path.join(__dirname, 'src')
+            loaders: ["babel"],
+            include: path.join(__dirname, "src")
         }, {
             test: /\.json$/,
-            loader: 'json'
+            loader: "json"
         }]
     },
     plugins: [
         new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /de|fr|hu|en-gb|en-ca/),
         new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
+            compressor: {
+                warnings: false,
+                screw_ie8: true
             }
         }),
+        // creates a stats.json
+        new StatsPlugin('webpack.stats.json', {
+            source: false,
+            modules: false
+        }),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
+            "process.env.NODE_ENV": JSON.stringify("production")
         })
     ]
 };
