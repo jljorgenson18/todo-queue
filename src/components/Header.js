3fd31af0189e82f623d7fd0249abce049391b4de
jljorgenson18 <jljorgenson18@gmail.com>
2016-11-22 21:17:44 -0600

    Cleaning up header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import React, { Component } from "react";
+import { grey50, grey800 } from "material-ui/styles/colors";
 
 const styles = {
     header: {
-        width: '100%',
-        backgroundColor: '#444',
-        color: '#FFF',
+        backgroundColor: grey800,
+        color: grey50,
         padding: 16,
-        display: 'flex',
-        justifyContent: 'center'
+        display: "flex",
+        justifyContent: "flex-start"
+    },
+    title: {
+        margin: 0,
+        fontWeight: "normal"
     }
 };
 
 class Header extends Component {
     render() {
         return (
-            <div id='header' style={ styles.header }>
-              <h1>The Todo Queue</h1>
+            <div id="header" style={ styles.header }>
+              <h1 style={ styles.title }>The Todo Queue</h1>
             </div>
             );
     }
